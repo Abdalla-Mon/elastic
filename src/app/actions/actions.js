6de36db23a8994ea5ae0ec4e70c12c5428616340
Abdalla Abdelsabour <abdotlos60@gmail.com>
2024-03-12 02:29:02 +0200

    edit the data fetching

--- src/app/actions/actions.js
+++ src/app/actions/actions.js
@@ -1,9 +1,40 @@
 "use server"
+import { Client } from "@elastic/elasticsearch";
 
-export async function handleSearch(search){
-    const data= await fetch(`https://elastic-ten.vercel.app/?q=${search}`)
-          .then(response=>response.json())
-          .then(data=> data
-          )
-    return await data;
+const client = new Client({
+    node: " https://6aec9ca46ed5408d922a6b08e0459821.us-central1.gcp.cloud.es.io:443",
+    auth: {
+        apiKey: "OW5tOUw0NEIxeUQzcUxUUXhuQmI6b2hzY0xwUHZSNXlBTkFQc3lvbnhVQQ==",
+    },
+});
+
+export async function handleSearch(q){
+    const elasticSearchParams = {
+        index: "main",
+        size: 20,
+        body: {
+            query: {
+                multi_match: {
+                    query: q,
+                    fields: ["abstract", "title"],
+                },
+            },
+        },
+    };
+    const data = await client.search(elasticSearchParams);
+    const documents = data.hits.hits.map((hit) => hit._source);
+
+    return documents;
+    // const response = await fetch(`https://elastic-ten.vercel.app/?q=${search}`);
+    // if (!response.ok) {
+    //     console.error("HTTP error! status: ", response.status);
+    //     console.error("The actual response is: ", await response.text());
+    //     return;
+    // } else if (!response.headers.get("content-type").includes("application/json")) {
+    //     console.error("The response is not JSON. The actual response is: ", await response.text());
+    //     return;
+    // }
+    //
+    // const data = await response.json();
+    // console.log(data)
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,7 @@
-// "use client"
-// import {useState} from "react";
-// import {handleSearch} from "@/app/actions/actions";
-//
-// export default function Home() {
-//     const [search,setSearch]=useState("")
-//
-//     const  handleInputChange =async (e) => {
-//         await handleSearch(e.target.value).then(data => {
-//             setSearch(data);
-//         });
-//     }
-//     return (
-//           <main>
-//               <div>
-//                   <form noValidate>
-//                       <input type="text" placeholder="Search" onChange={handleInputChange} />
-//                   </form>
-//               </div>
-//           </main>
-//     );
-// }
 import ElasticSearch from "@/app/components/ElasticSearch/ElasticSearch";
 
-export default function Home() {
+
+export default async function Home() {
+
     return <ElasticSearch />
 }
