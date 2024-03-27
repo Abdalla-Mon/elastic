17366eae1bedbfbc538d5df75017821bb6656e31
Abdalla Abdelsabour <abdotlos60@gmail.com>
2024-03-27 15:11:49 +0200

    Removed the extra spaces

--- src/app/components/ElasticSearch/ElasticSearchFilters/ElasticSearchField.jsx
+++ src/app/components/ElasticSearch/ElasticSearchFilters/ElasticSearchField.jsx
@@ -26,7 +26,10 @@ export default function ElasticSearchField() {
         fullWidth
       />
       <Button
-        onClick={() => setSearch(searchValue)}
+        onClick={() => {
+          const trimmedValue = searchValue.trim();
+          setSearch(trimmedValue);
+        }}
         className="py-3 bg-blue-500 text-white px-5 hover:bg-blue-400" // Tailwind CSS classes
       >
         Search
